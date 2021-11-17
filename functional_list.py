53b10bab4ace8ef0199df534abc0d7bff21d19fc
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-17 11:46:16 +0000

    Update functional_list.py

--- functional_list.py
+++ functional_list.py
@@ -2,13 +2,17 @@ my_list=["i", "d", "l", "e"]
 
 print(my_list) #prints['i','d','l','e']
 
-del my_list[2:4]
+del my_list[2:4] # deletes 'l' and 'e' from list leaving just ['i', 'd']
 
-my_list[1] = "n"
+print(my_list)
 
-new_list = ["s"] + my_list
+my_list[1] = "n" # replaces 'd' with 'n' to give ['i', 'n']
 
-new_list.append("g")
+new_list = ["s"] + my_list # adds 's' to start of a new list called new_list ['s', 'i', 'n']
+
+print(new_list)
+
+new_list.append("g") # adds 'g' to end of new_list to give ['s', 'i', 'n', 'g']
 
 print(my_list) # prints ['i','n']
 print(new_list) #prints ['s','i','n','g']
