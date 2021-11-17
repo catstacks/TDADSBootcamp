9138388d90b076d55ac30f448daedc3a2537a8c3
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-17 10:25:31 +0000

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,16 @@
+def convert_to_kg(pounds):
+    kilogram = pounds / 2.2
+    return kilogram
+
+print(convert_to_kg(5))
+
+def convert_to_celsius(temp):
+    degree_celsius = (temp-32)*(5/9)
+    return degree_celsius
+
+print(convert_to_celsius(100))
+
+
+
+
+
